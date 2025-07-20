833f883aea320e7562febc44902250a789bd68f1
alexduzi <duzihd@gmail.com>
2025-07-20 19:12:33 -0300

    next greater element and stack generic implementation

--- stack/main.go
+++ stack/main.go
@@ -2,8 +2,79 @@ package main
 
 import "fmt"
 
+type Stack[T int | string] struct {
+	items []T
+}
+
+func (s *Stack[T]) Push(item T) {
+	s.items = append(s.items, item)
+}
+
+func (s *Stack[T]) Pop() T {
+	if s.IsEmpty() {
+		var initial T
+		return initial
+	}
+
+	lastIdx := len(s.items) - 1
+	last := s.items[lastIdx]
+	s.items = s.items[:lastIdx]
+	return last
+}
+
+func (s *Stack[T]) Peek() T {
+	if s.IsEmpty() {
+		var initial T
+		return initial
+	}
+	return s.items[len(s.items)-1]
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.items) == 0
+}
+
+func (s *Stack[T]) Length() int {
+	return len(s.items)
+}
+
 func main() {
-	stack := new(Stack)
+	arr := []int{1, 4, 5, 6, 1}
+
+	reverseArr := nextGreaterElement(arr)
+
+	for _, value := range reverseArr {
+		fmt.Printf("%v\t", value)
+	}
+
+}
+
+func nextGreaterElement(arr []int) []int {
+	length := len(arr)
+	result := make([]int, length)
+	stack := Stack[int]{}
+
+	for i := length - 1; i >= 0; i-- {
+		if !stack.IsEmpty() {
+			for !stack.IsEmpty() && stack.Peek() <= arr[i] {
+				stack.Pop()
+			}
+		}
+
+		if stack.IsEmpty() {
+			result[i] = -1
+		} else {
+			result[i] = stack.Peek()
+		}
+
+		stack.Push(arr[i])
+	}
+
+	return result
+}
+
+func StackOperations() {
+	stack := Stack[string]{}
 
 	for i := 0; i < 100; i++ {
 		stack.Push(fmt.Sprintf("Item %v", i))
