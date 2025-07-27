9ce2249a23d787ee624a6a3a9cc40fba43e4d4ec
alexduzi <duzihd@gmail.com>
2025-07-26 22:03:57 -0300

    two sum leet code using map as a hash solution

---
+++ two_sum/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))
+}
+
+func twoSum(nums []int, target int) []int {
+	hasher := map[int]int{}
+	for index, value := range nums {
+		if idxM, ok := hasher[value]; ok {
+			return []int{idxM, index}
+		}
+		hasher[target-value] = index
+	}
+	return []int{}
+}
