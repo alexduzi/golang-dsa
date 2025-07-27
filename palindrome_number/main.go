dccb2a35e945540c476dee3191f91b519d4dff49
alexduzi <duzihd@gmail.com>
2025-07-26 23:04:23 -0300

    is palindrome using reverse digit technique

---
+++ palindrome_number/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(isPalindrome(121))
+}
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	reverse := 0
+	xcopy := x
+
+	for x > 0 {
+		reverse = (reverse * 10) + (x % 10)
+		x = x / 10
+	}
+	return reverse == xcopy
+}
