bda9306d9e4c70b5d8b71b72aa91865489caff06
alexduzi <duzihd@gmail.com>
2025-07-26 23:26:01 -0300

    explanations about the reverse digits

--- palindrome_number/main.go
+++ palindrome_number/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(isPalindrome(121))
+	fmt.Println(isPalindrome(1221))
 }
 
 func isPalindrome(x int) bool {
@@ -19,3 +19,11 @@ func isPalindrome(x int) bool {
 	}
 	return reverse == xcopy
 }
+
+// reverse digits technique
+// You need mod 10 and divide by 10 because we're working in base 10 (decimal)
+// Number: 1234
+// Step 1: Take 4 (1234 % 10), remove it (1234/10 = 123), reverse = 4
+// Step 2: Take 3 (123 % 10), remove it (123/10 = 12), reverse = 4*10 + 3 = 43
+// Step 3: Take 2 (12 % 10), remove it (12/10 = 1), reverse = 43*10 + 2 = 432
+// Step 4: Take 1 (1 % 10), remove it (1/10 = 0), reverse = 432*10 + 1 = 4321
