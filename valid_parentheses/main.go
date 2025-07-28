1e986100ac0893a9af32ca42a27ee38fdbba61f6
alexduzi <duzihd@gmail.com>
2025-07-27 21:29:42 -0300

    another valid parenthesis implementation

--- valid_parentheses/main.go
+++ valid_parentheses/main.go
@@ -38,6 +38,7 @@ func (s *Stack) Length() int {
 
 func main() {
 	fmt.Println(isValidParentheses("((({{{}}}[])))"))
+	fmt.Println(isValid("()"))
 }
 
 func isValidParentheses(s string) bool {
@@ -64,4 +65,23 @@ func isValidParentheses(s string) bool {
 		}
 	}
 	return true
+}
+
+func isValid(s string) bool {
+	stack := []rune{} // Stack for opening brackets
+	hash := map[rune]rune{')': '(', ']': '[', '}': '{'}
+
+	for _, char := range s {
+		if match, found := hash[char]; found {
+			// Check if stack is non-empty and matches
+			if len(stack) > 0 && stack[len(stack)-1] == match {
+				stack = stack[:len(stack)-1] // Pop
+			} else {
+				return false // Invalid
+			}
+		} else {
+			stack = append(stack, char) // Push opening bracket
+		}
+	}
+	return len(stack) == 0 // Valid if stack is empty
 }
