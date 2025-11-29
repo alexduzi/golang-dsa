03a808960c08432fa4f71892e97373cffd28c237
Alex Duzi <duzihd@gmail.com>
2025-11-29 11:37:10 -0300

    changing to map[byte]int to avoid conversions

--- roman_to_int/main.go
+++ roman_to_int/main.go
@@ -8,14 +8,14 @@ func main() {
 
 func romanToInt(s string) int {
 	// create a map with all symbols and respective values
-	romanMap := make(map[string]int)
-	romanMap["I"] = 1
-	romanMap["V"] = 5
-	romanMap["X"] = 10
-	romanMap["L"] = 50
-	romanMap["C"] = 100
-	romanMap["D"] = 500
-	romanMap["M"] = 1000
+	romanMap := make(map[byte]int)
+	romanMap['I'] = 1
+	romanMap['V'] = 5
+	romanMap['X'] = 10
+	romanMap['L'] = 50
+	romanMap['C'] = 100
+	romanMap['D'] = 500
+	romanMap['M'] = 1000
 
 	result := 0
 	size := len(s)
@@ -23,14 +23,14 @@ func romanToInt(s string) int {
 	for i := 0; i < size; i++ {
 		// if the current value is less than the next
 		// subtract current from net and add to result
-		if i+1 < size && romanMap[string(s[i])] < romanMap[string(s[i+1])] {
-			result += romanMap[string(s[i+1])] - romanMap[string(s[i])]
+		if i+1 < size && romanMap[s[i]] < romanMap[s[i+1]] {
+			result += romanMap[s[i+1]] - romanMap[s[i]]
 
 			// skip to the next symbol
 			i++
 		} else {
 			// if not, add the current value to result
-			result += romanMap[string(s[i])]
+			result += romanMap[s[i]]
 		}
 	}
 	return result
