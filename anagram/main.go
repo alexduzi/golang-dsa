f440c9bc2993a13849f3b15816cd1bc7cb03b68e
Alex Duzi <duzihd@gmail.com>
2026-03-15 21:17:26 -0300

    anagram

--- anagram/main.go
+++ anagram/main.go
@@ -11,6 +11,9 @@ func main() {
 
 	println(isAnagram2("anagram", "nagaram"))
 	println(isAnagram2("rat", "cat"))
+
+	println(isAnagram3("anagram", "nagaram"))
+	println(isAnagram3("rat", "cat"))
 }
 
 // anagram é um texto formado pelo rearanjo das letras em um texto diferente
@@ -53,4 +56,27 @@ func isAnagram2(s, t string) bool {
 	tSorted := strings.Join(tSplit, "")
 
 	return sSorted == tSorted
+}
+
+// tabela unicode
+func isAnagram3(s, t string) bool {
+
+	if len(s) != len(t) {
+		return false
+	}
+
+	var table [256]int
+
+	for i := 0; i < len(s); i++ {
+		table[s[i]]++
+		table[t[i]]--
+	}
+
+	for _, v := range table {
+		if v != 0 {
+			return false
+		}
+	}
+
+	return true
 }
