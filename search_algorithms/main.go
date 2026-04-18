bbe0ac5a462212e4be6d7ea402d3ac92d23f85e3
Alex Duzi <duzihd@gmail.com>
2026-04-18 18:58:22 -0300

    binary search recursive

--- search_algorithms/main.go
+++ search_algorithms/main.go
@@ -8,7 +8,10 @@ import (
 func main() {
 	numbers := []int{1, 20, 30, 5, 10, 17, 15}
 	index := binarySearchIter(numbers, 10)
-	fmt.Printf("%v", index)
+	fmt.Printf("%v\n", index)
+
+	index2 := binarySearchRecursive(numbers, 10, 0, len(numbers)-1)
+	fmt.Printf("%v\n", index2)
 }
 
 // busca sequencial
@@ -40,4 +43,21 @@ func binarySearchIter(arr []int, e int) int {
 		}
 	}
 	return -1
+}
+
+func binarySearchRecursive(arr []int, e, low, high int) int {
+
+	if low > high {
+		return -1
+	}
+
+	middle := int(math.Floor(float64((low + high) / 2)))
+
+	if e == arr[middle] {
+		return middle
+	} else if e < arr[middle] {
+		return binarySearchRecursive(arr, e, low, middle-1)
+	} else {
+		return binarySearchRecursive(arr, e, middle+1, high)
+	}
 }
