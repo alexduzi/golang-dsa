8a5b82aefd50f598b5b7183ce555a22a33fb1a69
Alex Duzi <duzihd@gmail.com>
2026-05-04 20:14:23 -0300

    refactoring

--- bubble_sort/main.go
+++ bubble_sort/main.go
@@ -10,13 +10,18 @@ func bubbleSort(nums []int) []int {
 	N := len(nums)
 
 	for i := 0; i < N; i++ {
-		for j := 0; j < N-1; j++ {
+		swapped := false
+		for j := 0; j < N-1-i; j++ {
 			if nums[j] > nums[j+1] {
 				// aux := nums[j]
 				// nums[j] = nums[j+1]
 				// nums[j+1] = aux
 				nums[j], nums[j+1] = nums[j+1], nums[j]
+				swapped = true
 			}
+		}
+		if !swapped {
+			break
 		}
 	}
 	return nums
