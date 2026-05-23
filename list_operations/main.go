47ab038c5b363904d9a18c427d5ec963504c0288
Alex Duzi <duzihd@gmail.com>
2026-05-23 11:42:37 -0300

    print linked list

--- list_operations/main.go
+++ list_operations/main.go
@@ -11,22 +11,24 @@ func main() {
 	linkedList.AddAtEnd(5)
 
 	linkedList.PrintLinkedList()
+
+	fmt.Printf("Tamanho da lista: %v", linkedList.GetSize())
 }
 
 type Node struct {
-	Value int
-	Next  *Node
+	value int
+	next  *Node
 }
 
 func NewNode(value int) *Node {
 	return &Node{
-		Value: value,
+		value: value,
 	}
 }
 
 type LinkedList struct {
-	Head *Node
-	Size int
+	head *Node
+	size int
 }
 
 func NewLinkedList() *LinkedList {
@@ -37,32 +39,36 @@ func (l *LinkedList) AddAtEnd(value int) {
 	node := NewNode(value)
 
 	// tratar se a lista está vazia
-	if l.Head == nil {
-		l.Head = node
-		l.Size++
+	if l.head == nil {
+		l.head = node
+		l.size++
 		return
 	}
 
 	// pega o nó atual e percorre a lista até achar o final, que vai ser nulo
-	current := l.Head
-	for current.Next != nil {
-		current = current.Next
+	current := l.head
+	for current.next != nil {
+		current = current.next
 	}
 
-	current.Next = node
-	l.Size++
+	current.next = node
+	l.size++
 }
 
 func (l *LinkedList) PrintLinkedList() {
-	if l.Head == nil {
+	if l.head == nil {
 		fmt.Printf("Lista vazia!\n")
 		return
 	}
 
-	current := l.Head
+	current := l.head
 
 	for current != nil {
-		fmt.Printf("%v\n", current.Value)
-		current = current.Next
+		fmt.Printf("%v\n", current.value)
+		current = current.next
 	}
+}
+
+func (l *LinkedList) GetSize() int {
+	return l.size
 }
