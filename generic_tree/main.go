1d7df48c999194556a367e13dd5b2bba60a63067
Alex Duzi <duzihd@gmail.com>
2026-06-07 12:56:42 -0300

    generic tree operations

--- generic_tree/main.go
+++ generic_tree/main.go
@@ -9,21 +9,49 @@ type Position[T any] interface {
 }
 
 type Node[T any] struct {
-	element  T
+	_element T
 	parent   *Node[T]
 	children []*Node[T]
 }
 
 func NewNode[T any](element T, parent *Node[T]) *Node[T] {
 	return &Node[T]{
-		element:  element,
+		_element: element,
 		parent:   parent,
 		children: []*Node[T]{},
 	}
 }
 
-func (node *Node[T]) Element() T {
-	return node.element
+func (node *Node[T]) element() T {
+	return node._element
+}
+
+func (node *Node[T]) getParent() *Node[T] {
+	return node.parent
+}
+
+func (node *Node[T]) getChildren() []*Node[T] {
+	return node.children
+}
+
+func (node *Node[T]) addChild(child *Node[T]) {
+	node.children = append(node.children, child)
+	child.parent = node
+}
+
+func (node *Node[T]) removeChild(child *Node[T]) {
+	children := make([]*Node[T], 0, len(node.children)-1)
+	for _, c := range node.children {
+		if c != child {
+			children = append(children, c)
+		}
+	}
+	child.parent = nil
+	node.children = children
+}
+
+func (node *Node[T]) setElement(element T) {
+	node._element = element
 }
 
 func (node *Node[T]) isLeaf() bool {
