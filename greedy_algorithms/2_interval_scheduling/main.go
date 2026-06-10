b2d2588e1eac4a71742c0e8e53b90878318eb697
Alex Duzi <duzihd@gmail.com>
2026-06-10 18:51:44 -0300

    greedy algorithms: interval scheduling

---
+++ greedy_algorithms/2_interval_scheduling/main.go
@@ -0,0 +1,8 @@
+package main
+
+// Suponha que tenhamos N tarefas para realizar, cada uma com um tempo de começo
+// e um tempo de fim. Ao começar uma tarefa, a executamos até o seu fim, e só então
+// podemos começar outra. Qual o número máximo de tarefas que é possível realizar?
+func main() {
+
+}
